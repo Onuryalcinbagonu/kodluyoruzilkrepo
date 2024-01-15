0a2f5200af271d292395f8343d7932d8a7e9da0f
Onur Yalcin Bagonu <oybagonu@gmail.com>
2024-01-15 22:55:14 +0300

    C# Ödev 12 yapıldı.

---
+++ C#/Ödev 12/Program.cs
@@ -0,0 +1,50 @@
+﻿//out parametreler
+string sayi="999";
+
+bool sonuc=int.TryParse(sayi,out int outSayi);
+//int değişkenine dönüşebilir mi sorusuna TryParse tarafından cevap döner. out ile o sayiya dönüşürse outSayi değişkene atama yapar. Eğer cevap false ise outSayi 0 değerini alır.
+
+if(sonuc)
+{
+    System.Console.WriteLine("Başarılı");
+    System.Console.WriteLine(outSayi);
+}
+else
+{
+    System.Console.WriteLine("Başarısız");
+    System.Console.WriteLine(outSayi);
+
+}
+
+Metotlar instance=new Metotlar();
+instance.topla(4,5,out int toplamSonucu);
+Console.WriteLine(toplamSonucu);
+
+//Metot Aşırı Yükleme - Overloading
+int ifade=999;
+instance.ekranaYazdir(ifade);
+instance.ekranaYazdir("Onur","Yalcin");
+
+
+class Metotlar
+{
+
+    public void topla(int a, int b,out int toplam)
+    {
+        toplam=a+b;
+    }
+    public void ekranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void ekranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void ekranaYazdir(string veri,string veri2)
+    {
+        Console.WriteLine(veri+veri2);
+    }
+}
