3b2ea9bad2f4b8d9d9adf1c87d6e3c46a78a5819
Onur Yalcin Bagonu <oybagonu@gmail.com>
2024-02-01 19:35:17 +0300

    C# Ödev 26 (Enum) yapıldı.

---
+++ C#/Ödev 26/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Cumartesi);
+
+int sicaklik=24;
+
+if(sicaklik<=(int)HavaDurumu.Normal)
+{
+    Console.WriteLine("Dışarıya çıkmak için havanın ısınması beklenebilir.");
+}
+else if(sicaklik >= (int)HavaDurumu.Sicak)
+{
+    Console.WriteLine("Dışarıya çıkmak için hava sıcak.");
+}
+else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik <(int)HavaDurumu.CokSicak)
+{
+    Console.WriteLine("Dışarıya çıkılabilir.");
+}
+
+enum Gunler 
+
+{
+    Pazartesi=1,
+    // Gunlerin numeric değerini 1 den başlat
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma=23,
+    Cumartesi,
+    Pazar
+ }
+
+ enum HavaDurumu
+ {
+    Soguk=5,
+    Normal=20,
+    Sicak=25,
+    CokSicak=30
+ }
